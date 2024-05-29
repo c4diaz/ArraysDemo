e8cb8096cf557415aec5b8559e20c037cca41772
c4diaz <CARLOS DIAZ@DESKTOP-91AU5N9>
2024-05-29 11:06:27 +0200

    decalaracion de una array enteros

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,12 +2,24 @@ package com.example;
 
 /**
  * Hello world!
- *
+ *[]
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //Declaracion de un array
+    	int[] numeros; // Declaramos que numeros es un array de valores enteros.
+    	
+    	// como agregarle valores al array de numero;
+    	// Primeramente hay que 
+    	
+    	numeros = new int [10];
+    	
+    	numeros[0] = 1;
+    	numeros[1] = 2;
+    	numeros[2] = 3;
+    	//y asi, hasta el indice 9, por que del cero al nueve hay 10 elementos.
+    	;
     }
 }
