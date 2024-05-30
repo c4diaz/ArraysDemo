432224b42ce9ac44639ff5d99522b7243c7837bf
c4diaz <CARLOS DIAZ@DESKTOP-91AU5N9>
2024-05-30 13:29:01 +0200

    solucion al ejercicio 1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,15 @@
 package com.example;
 
-import javax.swing.JOptionPane;
 
 /**
- * Hello world!
+ * Actividad del Jueves 30 de Mayo
+
+Ejercicio # 1.
+
+Modificar el proyecto ArraysDemo para que reciba 4 argumentos y el cuarto sea un valor de tipo double para
+asignarlo a una variable static de tipo double en la clase que contiene el metodo main.
+
+Mostrar el valor de la variable que a la cual le hemos asignado el argumento.
  *[]
  */
 public class App 
@@ -11,104 +17,34 @@ public class App
 	
 	private static int ARRAY_SIZE;
 	private static String nombre;
+	private static double cuartoArgumento;
 	
     public static void main( String[] args )
     {
-        //como declara el array de la mejor forma.
-    	
-    	int[] numeros = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-    	
-    	//un array de nombres seria:
-    	
-    	String[] nombres = {"Tamara", "Jessica", "Carlos", "Adrian", "Maria Lopez", "Rosa", "Pablo", "Ivan", "Maria Jose", "Marly"};
-    	
-    	//para mostrar por consola los elementos del array de nombres 
-    	
-    	
-    	
-    	
-    	/*Manejo de los argumentos que recibe el metodo MAIN, es decir, la app cuando se ejecuta, por que la App cuando
-    	 * se ejecuta recibe como parametro un array de argumentos de tipo string (String[] args)
-    	 * 
-    	 * ¿Donde se especifican los argumentos que recibe la app en el IDE Eclipse?
-    	 * Rta: En la configuracion de las opciones de ejecucion.*/
-    	
-    	
-    	/*//Para comprobar si el programa esta recibiendo los argumentos 
+
     	if(args.length == 0 ) {
     		System.out.println("No se han recibido los argumentos esperados");}
     	
-    	else if (args.length != 3){
+    	else if (args.length != 4){
     		System.out.println("No se han recibido tres argumentos");}
-    	else 
-    		System.out.println("ya tenemos tres o mas argumentos");
-    		for(String argumento : args)
-    			System.out.println(argumento);*/
+    	
+    	else {
+    		System.out.println("ya tenemos cuatro o mas argumentos");}
     		
     	
-    		
-    		//parseInt
-    		ARRAY_SIZE = Integer.parseInt(args[1]) ; //"20"
-    		 System.out.println("El tamayño del array es : "  + ARRAY_SIZE);
-    		
-    		
-    		
-    		
+    	for(String argumento : args)
+    			System.out.println(argumento);
     		
     	
-    	// Esta VARIANTE #1 Implica utilizar una sentencia de control de flujo
-    	//para recorrer el array y lo veremos en la barra  "sentenciasdecontroldeflujo"
+    	ARRAY_SIZE = Integer.parseInt(args[1]);
+    		System.out.println("el segundo argumento es \n" + ARRAY_SIZE);
     	
     	
-    	//recorremos en bucle todos los nombres con la sentencia FOR de toda la vida o clasica .
-    	//for (declaramos la variable int = 0 "iniciar en 0 para tener en cuenta el inicio del array" ; 
-    	// despues del punto y coma va la condicion que tiene que cumplirse = (haremos la condicion para recorrer los nombres))
-    	// ; i aumenta desde cero sea menor hasta la cantidad del array. es decir con el .length que indica que recorrera los nombres 
-    	//la condicion de ; i = i + 1 empezara a recorrer en el bucle, lo notaremos asi= i++
-    	
-    	//¿que diferenia hay entre utilizar el operador i++ o ++i?
-    	// para solucionar este problema clasico de un lenguaje de programacion utilizaremos un JSHELL, que es un como un shell de python
-    	// que antes java no tenia.
-    	// ++j no es lo mismo que j++ ver imagenes power shell
-    	
-    	
-    
-    	
-    	 for(int i = 0; i < nombres.length; i++) {
-    		 
-    		//System.out.println(nombres[i]);
+    	cuartoArgumento = Double.parseDouble(args[3]);
+    		System.out.println("el cuarto  argumento es \n" + cuartoArgumento);
     		
-    	 }
-    	 
-    	//CUANDO UTILIZAR ESTE FOR CLASICO= cuando queremos recorrer el indice y queremos recorrer un indice especificado. ejemplo:
-    	 
-    	 //	 for(int i = 0; i < numeros.length; i++) {
-    	 		// if (i % 2 == 0) // == es el simbolo = 
-					//System.out.println("el elemento de indice " + i + ", es par");
-    	 	//	 else
-    	 			//System.out.println("el elemento de indice " + i + ", es impar");
-    	 	// }
     	
-    	 /* solamente mostrar los nombres que tienen mas de 4 caracteres*/
-    	 
-    	// for(int i = 0; i < nombres.length; i++) {
-    		 //lengt() entre parentesis es un metodo
-    	//	 if (nombres[i].length() > 4) {
-    			// System.out.println(nombres[i]);
-    		 }
-    		 
-     		
-     		
-     	// }
-    	 
-    	
-    	//recorrido con FOR mejorado en ocasiones MAL llamado for each (por cada) y digo mal llamado por que la sentencia 
-    	//forEach existe.
-    	
-    	 
-    	 
-    	
-  //  }
+   }
 }
 
 
